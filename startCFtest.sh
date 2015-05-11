0eeaa2515417217e9af42805eb1303e108c5b621
Jordi Cenzano <jcenzano@brightcove.com>
2015-05-10 20:15:56 -0700

    ffmpeg test file realtime adjustments

--- startCFtest.sh
+++ startCFtest.sh
@@ -39,8 +39,8 @@ fi
 
 #Stream to A (depending on encoding settings the wowza used resources can change A LOT)
 echo "Running ffmpeg to wowza A, logs in ./log/ffmpegA"
-ffmpeg -f lavfi -i testsrc=duration=36000:size=320x240:rate=25 -f lavfi -i "sine=frequency=1000:duration=36000" -i ~/Pictures/P.png -filter_complex 'overlay=10:main_h-overlay_h-10' -pix_fmt yuv420p -c:v libx264 -b:v 500k -profile:v baseline -preset ultrafast -c:a libfaac -q:a 50 -f flv "rtmp://$wuser:$wpass@$urlwA:1935/liveorigin/$streamName" > ./log/ffmpegA.log 2> ./log/ffmpegA_err.log &
+ffmpeg -f lavfi -re -i testsrc=duration=36000:size=320x250:rate=25 -f lavfi -re -i "sine=frequency=1000:duration=36000:sample_rate=44100" -i ~/Pictures/P.png -filter_complex 'overlay=10:main_h-overlay_h-10' -pix_fmt yuv420p -c:v libx264 -b:v 500k -g 25 -profile:v baseline -preset veryfast -c:a libfaac -b:a 96k -f flv "rtmp://$wuser:$wpass@$urlwA:1935/liveorigin/$streamName" > ./log/ffmpegA.log 2> ./log/ffmpegA_err.log &
 
 #Stream to B
 echo "Running ffmpeg to wowza B, logs in ./log/ffmpegB"
-ffmpeg -f lavfi -i testsrc=duration=36000:size=320x240:rate=25 -f lavfi -i "sine=frequency=1000:duration=36000" -i ~/Pictures/P.png -filter_complex 'overlay=10:main_h-overlay_h-10' -pix_fmt yuv420p -c:v libx264 -b:v 500k -profile:v baseline -preset ultrafast -c:a libfaac -q:a 50 -f flv "rtmp://$wuser:$wpass@$urlwB:1935/liveorigin/$streamName" > ./log/ffmpegB.log 2> ./log/ffmpegB_err.log &
+ffmpeg -f lavfi -re -i testsrc=duration=36000:size=320x250:rate=25 -f lavfi -re -i "sine=frequency=1000:duration=36000:sample_rate=44100" -i ~/Pictures/B.png -filter_complex 'overlay=10:main_h-overlay_h-10' -pix_fmt yuv420p -c:v libx264 -b:v 500k -g 25 -profile:v baseline -preset veryfast -c:a libfaac -b:a 96k -f flv "rtmp://$wuser:$wpass@$urlwB:1935/liveorigin/$streamName" > ./log/ffmpegB.log 2> ./log/ffmpegB_err.log &
